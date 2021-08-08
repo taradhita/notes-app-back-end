c97f4f1258571d6cbd8aac040c7c337490e6c1cd
taradhita <taradhita@gmail.com>
2021-08-08 10:12:23 +0800

    postgres database implemented

--- src/services/inMemory/NotesServices.js
+++ src/services/inMemory/NotesServices.js
@@ -40,7 +40,7 @@ class NotesService {
     return note;
   }
 
-  editNote(id, { title, body, tags }) {
+  editNoteById(id, { title, body, tags }) {
     const index = this._notes.findIndex((note) => note.id === id);
 
     if (index === -1) {
@@ -58,7 +58,7 @@ class NotesService {
     };
   }
 
-  deleteNote(id) {
+  deleteNoteById(id) {
     const index = this._notes.findIndex((note) => note.id === id);
 
     if (index === -1) {
--- src/services/postgres/NotesServices.js
+++ src/services/postgres/NotesServices.js
@@ -1,30 +1,79 @@
 const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const InvariantError = require('../../error/InvariantError');
+const { mapDBToModel } = require('../../utils');
+const NotFoundError = require('../../error/NotFoundError');
 
 class NotesService {
   constructor() {
     this._pool = new Pool();
   }
 
-  addNote({ title, body, tags }) {
+  async addNote({ title, body, tags }) {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
-    const newNote = {
-      title, tags, body, id, createdAt, updatedAt,
+    const query = {
+      text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+      values: [id, title, body, tags, createdAt, updatedAt],
     };
 
-    this._notes.push(newNote);
+    const result = await this._pool.query(query);
 
-    const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
-
-    if (!isSuccess) {
+    if (!result.rows[0].id) {
       throw new InvariantError('Catatan gagal ditambahkan');
     }
 
-    return id;
+    return result.rows[0].id;
+  }
+
+  async getNotes() {
+    const result = await this._pool.query('SELECT * FROM notes');
+    return result.rows.map(mapDBToModel);
+  }
+
+  async getNoteById(id) {
+    const query = {
+      text: 'SELECT * FROM notes WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Catatan tidak ditemukan');
+    }
+
+    return result.rows.map(mapDBToModel)[0];
+  }
+
+  async editNoteById(id, { title, body, tags }) {
+    const updatedAt = new Date().toISOString();
+
+    const query = {
+      text: 'UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id',
+      values: [title, body, tags, updatedAt, id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
+    }
+  }
+
+  async deleteNoteById(id) {
+    const query = {
+      text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
+    }
   }
 }
 
